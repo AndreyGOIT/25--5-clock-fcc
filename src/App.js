367d06a2cedc5eb85cd2ba9411b2e7de33566c2c
AndreyGOIT <ae.borga@gmail.com>
2025-06-05 16:06:34 +0300

    add container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">25-5 Clock</div>;
+  return (
+    <div className="container d-flex flex-column align-items-center">
+      <h1>25-5 Clock</h1>{" "}
+      <div className="row">
+        <div id="break-label" className="col-sm-4">
+          Break Length
+        </div>
+        <div id="session-label" className="col-sm-4">
+          Session Length
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
